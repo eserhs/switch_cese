cf54d056408e2c9e983609dedaa33cf475d7db84
Hüseyin Mustafa <huseyinbanna216@gmail.com>
2021-01-25 23:24:05 +0300

    Proje dosyası ekle.

---
+++ switch cese/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace switch_cese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  gun1 = 10;
+            int  gun2 = 5;
+
+            switch (gun1)
+            {
+
+                case  (10):
+                    for (int i = 0; i  < 10 ; i++)
+                    {
+                        Console.WriteLine(10 - i);
+                    }
+                    break;
+                case 5 :
+                    Console.WriteLine(5-4 ) ;
+                    break;
+
+                default:
+                    Console.WriteLine("yok valla abi olmadı ");
+                    break;
+                   
+            }
+        }
+    }
+}
